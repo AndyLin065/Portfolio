6f22cb8d15359227dba8512d0d96686c1c5b59a2
AndyLin065 <132158174+AndyLin065@users.noreply.github.com>
2025-11-03 16:29:06 -0500

    Create Crypto_API.py

---
+++ Crypto_API.py
@@ -0,0 +1,28 @@
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
+#API Documentation: https://coinmarketcap.com/api/documentation/v1/#section/Quick-Start-Guide
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest' 
+
+parameters = {
+  'start':'1',
+  'limit':'15',
+  'convert':'USD'
+}
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': '4cc875e23fc24173964f728c775a143c',
+}
+
+session = Session()
+session.headers.update(headers)
+
+try:
+  response = session.get(url, params=parameters)
+  data = json.loads(response.text)
+  #print(data)
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+  print(e)
+
+type(data)
