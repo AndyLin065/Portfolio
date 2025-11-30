a3b0ed99720fb36784294f2a24f13fd83efe879a
AndyLin065 <132158174+AndyLin065@users.noreply.github.com>
2025-11-30 01:37:32 -0500

    Update DS_Random_Forest.py

--- DS_Random_Forest.py
+++ DS_Random_Forest.py
@@ -1,7 +1,10 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score, classification_report
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Dataset: Credit Card Fraud Detection
 # Download from: https://www.kaggle.com/datasets/mlg-ulb/creditcardfraud
